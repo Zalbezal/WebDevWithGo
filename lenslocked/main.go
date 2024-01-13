2bccfe2b1949758aaa250dfd4af19f3e3bca7e70
Zalbezal <63258565+Zalbezal@users.noreply.github.com>
2024-01-13 10:32:34 +0100

    Refactored Contact and FAQ page to Golang templates

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -11,10 +11,9 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tplPath := filepath.Join("templates", "home.gohtml")
-	tpl, err := template.ParseFiles(tplPath)
+	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
 		log.Printf("parsing template: %v", err)
 		http.Error(w, "there was an error parsing the template.", http.StatusInternalServerError)
@@ -26,28 +25,22 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "there was an error executing the template.", http.StatusInternalServerError)
 		return
 	}
-	
+
+}
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)
+
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jeppe-christiansen@hotmail.com\">jeppe-christiansen@hotmail.com</a>.</p>")
-	userID := chi.URLParam(r, "userID")
-	w.Write([]byte(fmt.Sprintf("%v", userID)))
-
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>FAQ Page</h1>")
-	fmt.Fprint(w, `<ul>
-		<li><b>Q: is this a course</b> A: No this is a test site</li>
-		<li><b>Q: Is it free</b> A: well yeah if you can find it</li>
-		<li><b>How do I contact support?</b> Email us - <a 
-		href="mailto:support@lenslocked.com">support@lenslocked.com</a></li>
-		</ul>
-		`)
+	tplPath := filepath.Join("templates", "faq.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func main() {
