2492391301e0aeb17e7325942f121b9ee041a540
Zalbezal <63258565+Zalbezal@users.noreply.github.com>
2024-01-20 11:42:38 +0100

    Created the users controller and moved the signup page over to it

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -23,8 +23,13 @@ func main() {
 	r.Get("/faq", controllers.FAQ(
 		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
-	r.Get("/signup", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))))
+	usersC := controllers.Users{}
+	usersC.Templates.New = views.Must(views.ParseFS(
+		templates.FS, 
+		"signup.gohtml", "tailwind.gohtml",
+	))
+	r.Get("/signup", usersC.New)
+
 	r.Get("/static", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "static.gohtml")))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
